4c773b3805e2194b3698d517dd2b03720e3dfdc2
n1KOLO55 <142680964+n1KOLO55@users.noreply.github.com>
2023-09-01 22:27:52 +0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+  });
+  
+  // Form submission handling
+ 
+//---------------------------------------------------------
+  const fadeElements = document.querySelectorAll('.fade-in');
+
+  function onEntry(entry) {
+    entry.forEach(change => {
+      if (change.isIntersecting) {
+       change.target.classList.add('element-show');
+      }
+    });
+  }
+  
+  let options = {
+    threshold: [0.5] };
+  let observer = new IntersectionObserver(onEntry, options);
+  let elements = document.querySelectorAll('.element-animation');
+  
+  for (let elm of elements) {
+    observer.observe(elm);
+  }
+
+
+
+//---------------------------------------------------------
+
+
+
+
+
+
